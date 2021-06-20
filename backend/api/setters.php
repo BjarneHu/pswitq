42496b868a3ce3d3164539c80ddc09662d0bf035
Ichrak Allagui <ichrakallagui@Ichraks-MacBook-Pro.local>
2021-06-20 16:11:42 +0200

    adjust php

--- backend/api/setters.php
+++ backend/api/setters.php
@@ -13,10 +13,10 @@ function set_order($conn, $data) {
     foreach($data['warencorb'] as $model) {
         $model_sql = sprintf("Insert into Bestellmodell (name, material_id, %s unit_price, quantity,
         volume, size,  color,  order_id)  VALUES ('%s', %d, %s %f, %d, %f, '%s', '%s', %d)", 
-        $model['post_process'] ? 'post_process_id,' : '',
+        !empty($model['post_process']) ? 'post_process_id,' : '',
         $model['file_name'], 
         $model['material'], 
-        $model['post_process'] ? $model['post_process'].',' : '', 
+        !empty($model['post_process']) ? $model['post_process'].',' : '', 
         $model['preis'], 
         $model['menge'], 
         $model['volumen'], 
