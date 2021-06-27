0a8a2f744a43e9dd0e213dfdefe356d943206fed
Ichrak Allagui <ichrakallagui@Ichraks-MacBook-Pro.local>
2021-06-27 10:56:39 +0100

    add file to db

--- backend/api/setters.php
+++ backend/api/setters.php
@@ -11,8 +11,11 @@ function set_order($conn, $data) {
     $conn->query($order_sql);
     $order_id = $conn->insert_id;
     foreach($data['warencorb'] as $model) {
-        $model_sql = sprintf("Insert into Bestellmodell (name, material_id, %s unit_price, quantity,
-        volume, size,  color,  order_id)  VALUES ('%s', %d, %s %f, %d, %f, '%s', '%s', %d)", 
+        //$fileData = addslashes(file_get_contents($model['model_file']));
+        $fileProperties = pathinfo($model['file_name']);
+        $model_sql = sprintf("Insert into Bestellmodell 
+        (name, material_id, %s unit_price, quantity, volume, size,  color,  order_id, model_file, file_type)  
+        VALUES ('%s', %d, %s %f, %d, %f, '%s', '%s', %d, '%s', '%s')", 
         !empty($model['post_process']) ? 'post_process_id,' : '',
         $model['file_name'], 
         $model['material'], 
@@ -22,10 +25,13 @@ function set_order($conn, $data) {
         $model['volumen'], 
         $model['xsize'].'x'.$model['ysize'].'x'.$model['zsize'], 
         $model['color'], 
-        $order_id
+        $order_id,
+        $model['model_file'],
+        $model['file_type']
         );
         $conn->query($model_sql);
     }
+    echo $conn->error;
     echo 'ok';
     die(); 
 }
